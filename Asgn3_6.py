aec698a6154da20e0e190c60edb5631b38ed4bd4
Swaminathan-Natarajan <swaminathan.n910@gmail.com>
2026-01-03 12:44:31 +0530

    Create Asgn3_6.py
    Type, Id and size in bytes

---
+++ Asgn3_6.py
@@ -0,0 +1,17 @@
+import sys
+
+num = 10
+name = "Swami"
+Marks = 99.99
+
+print(type(num))
+print(type(name))
+print(type(Marks))
+
+print(id(num))
+print(id(name))
+print(id(Marks))
+
+print(sys.getsizeof(num))
+print(sys.getsizeof(name))
+print(sys.getsizeof(Marks))
